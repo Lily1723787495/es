baaf1d3f5334a3082469f99ed619c658f1d3582e
d9e7381f <939770681@qq.com>
2019-04-04 09:34:17 +0800

    init

--- src/main/java/cn/edu/dgut/educationalsystem/controller/StudentApiController.java
+++ src/main/java/cn/edu/dgut/educationalsystem/controller/StudentApiController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,4 +36,9 @@ public class StudentApiController {
     }
 
 
+    @GetMapping("/courses")
+
+    public Result courseList(){
+        return ResultUtils.success();
+    }
 }
